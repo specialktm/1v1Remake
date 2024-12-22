351ebc8df075bbcf31851a8bf247f15a762ea587
ILoveMenOng <145188879+ILoveMenOng@users.noreply.github.com>
2024-12-22 02:44:16 -0600

    Update DrawMenu.cpp

--- cheat/source/cheat/renderer/Menu/DrawMenu.cpp
+++ cheat/source/cheat/renderer/Menu/DrawMenu.cpp
@@ -7,8 +7,8 @@ namespace cheat
 	void Submenus::Home()
 	{
 		Title("Home");
-		NewSub(ICON_FA_BINOCULARS" ESP", Menus::MenuESP);
-		NewSub(ICON_FA_COGS" Settings", Menus::MenuSettings);
+		NewSub("ESP", Menus::MenuESP);
+		NewSub("Settings", Menus::MenuSettings);
 	}
 
 }
